5dbf0cac97202a42914361cf698e8d8530e5e4b5
David <davejohngoddard@gmail.com>
2025-01-09 23:02:38 +0000

    Added app id

--- iac/ghrunners/remote_state.tf
+++ iac/ghrunners/remote_state.tf
@@ -1,6 +1,7 @@
 # Used to fetch the outputs from devopsutils statefile
 data "terraform_remote_state" "devopsutils" {
   backend = "azurerm"
+  use_oidc = true
 
   config = {
     storage_account_name = "stglbuksdevopstf"

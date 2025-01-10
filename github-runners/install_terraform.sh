1751acc46800eedad9c508cd6c0c8e019fbcc9d2
davejg10 <davejohngoddard@gmail.com>
2025-01-10 17:56:32 +0000

    feature/ghrunner-terraform-install (#4)
    This PR contains the following:
    - Installed Terraform in the image used for the gh runners
    - Changed build-push image workflow to use az acr commands so docker is no longer required and it can be run on my new agents

---
+++ github-runners/install_terraform.sh
@@ -0,0 +1,32 @@
+
+#!/bin/bash
+
+# Check if a version argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+
+VERSION=$1
+
+if ! unzip -v; then
+  echo "[INFO] unzip not found, running unzip install."
+
+  until sudo apt-get update && sudo apt-get install zip unzip --assume-yes
+  do
+      echo "[INFO] Waiting to acquire the dpkg frontend lock..."
+      sleep 5
+  done
+  echo "[INFO] unzip successfully installed!"
+  fi
+
+  terraform -version
+  if [ "$(terraform version -json | grep terraform_version  | awk -F'\"' '{print $4}')" == "$VERSION" ]; then
+  echo "[INFO] terraform $VERSION already installed, skipping installation"
+  else
+  echo "[INFO] terraform $VERSION version not found. Downloading and installing terraform"
+  wget "https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip" -O terraform.zip
+  sudo unzip -o "terraform.zip" -d /usr/local/bin
+  rm terraform.zip
+  terraform --version
+fi

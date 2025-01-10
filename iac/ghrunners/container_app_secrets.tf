ccc21376769f9061b6a8ab2e5377059633718a1b
David <davejohngoddard@gmail.com>
2025-01-10 02:23:46 +0000

    Repos

--- iac/ghrunners/container_app_secrets.tf
+++ iac/ghrunners/container_app_secrets.tf
@@ -3,9 +3,9 @@ resource "azurerm_key_vault_secret" "github_access_token" {
   value        = var.github_access_token
   key_vault_id = data.terraform_remote_state.devopsutils.outputs.key_vault_id
 
-  lifecycle {
-    ignore_changes = [
-      value  # Ignore changes to the 'value' attribute
-    ]
-  }
+  # lifecycle {
+  #   ignore_changes = [
+  #     value  # Ignore changes to the 'value' attribute
+  #   ]
+  # }
 }

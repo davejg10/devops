1b4fdb1e6c0e7126f33b94574a60251df83ebd37
David <davejohngoddard@gmail.com>
2025-01-13 22:54:03 +0000

    Sourcing between scripts

--- github-runners/install_script.sh
+++ github-runners/install_script.sh
@@ -11,7 +11,13 @@ for file in /etc/profile.d/*.sh; do
     fi
 done
 EOF
+
+echo
+
+echo "the shell iS"
 ps -p $$ -o comm=
+
+echo
 cat ~/.bashrc
 
 echo "Installing Terraform version $TERRAFORM_VERSION"
@@ -22,6 +28,9 @@ echo "Installing JDK version $JDK_VERSION"
 chmod +x install_jdk.sh
 ./install_jdk.sh $JDK_VERSION
 
+source ~/.bashrc
 echo "Installing Maven version $MVN_VERSION"
 chmod +x install_mvn.sh
 ./install_mvn.sh $MVN_VERSION
+
+source ~/.bashrc

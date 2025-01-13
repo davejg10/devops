d70700554d468960996300387a19ba6a5994e3ad
David <davejohngoddard@gmail.com>
2025-01-13 10:19:48 +0000

    Added ukranges to acr

--- iac/devopsutils/acr.tf
+++ iac/devopsutils/acr.tf
@@ -1,29 +1,35 @@
 locals {
-  uk_south_acr_azure_ip_ranges = "${path.cwd}/uk_south.json"
-  uk_west_acr_azure_ip_ranges = "${path.cwd}/uk_south.json"
+  
+  uk_south_acr_azure_ip_ranges = jsondecode(file("${path.module}/uk_south.json")).ip_ranges
+  # uk_west_acr_azure_ip_ranges = "${path.cwd}/uk_south.json"
 }
 
-resource "null_resource" "fetch_uk_south_acr_ips" {
-  triggers = {
-    always_run = timestamp()
-  }
-
-  provisioner "local-exec" {
-    # Logs into the Service principal that has subscription permission over the core network.
-    command = <<CMD
-      curl -o ips.json https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20250106.json
-      jq -r '.values[] | select(.name == "AzureContainerRegistry.UKSouth") | {ip_ranges: .properties.addressPrefixes}' ips.json > local.uk_south_acr_azure_ip_ranges
-      jq -r '.values[] | select(.name == "AzureContainerRegistry.UKWest") | {ip_ranges: .properties.addressPrefixes}' ips.json > local.uk_west_acr_azure_ip_ranges
-    CMD
-  }
+variable "uk_south_acr_azure_ip_ranges" {
+  
 }
+
+# variable "uk_south_acr_azure_ip_ranges" {
+#   type = string
+#   default = "uk_south.json"
+# }
+
+# resource "null_resource" "fetch_uk_south_acr_ips" {
+#   triggers = {
+#     always_run = timestamp()
+#   }
+
+#   provisioner "local-exec" {
+#     # Logs into the Service principal that has subscription permission over the core network.
+#     command = <<CMD
+#       curl -o ips.json https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20250106.json
+#       jq -r '.values[] | select(.name == "AzureContainerRegistry.UKSouth") | {ip_ranges: .properties.addressPrefixes}' ips.json > local.uk_south_acr_azure_ip_ranges
+#       jq -r '.values[] | select(.name == "AzureContainerRegistry.UKWest") | {ip_ranges: .properties.addressPrefixes}' ips.json > local.uk_west_acr_azure_ip_ranges
+#     CMD
+#   }
+# }
 
 
 resource "azurerm_container_registry" "devops" {
-  depends_on = [
-    null_resource.fetch_uk_south_acr_ips
-  ]
-
   name = "acr${var.environment_settings.environment}${var.environment_settings.region_code}${var.environment_settings.app_name}"
 
   resource_group_name     = data.azurerm_resource_group.rg.name
@@ -33,14 +39,13 @@ resource "azurerm_container_registry" "devops" {
 
   network_rule_set {
     default_action = "Deny"
-    
-    dynamic "ip_rule" {
-      for_each = jsondecode(file(local.uk_south-acr_azure_ip_ranges)).ip_ranges
-      content {
-        action = "Allow"
-        ip_range = ip_rule.value
-      }
-    }
+
+    ip_rule = [
+      for ip_range in local.uk_south_acr_azure_ip_ranges : {
+        action   = "Allow"
+        ip_range = ip_range
+      } if !strcontains(ip_range, ":")
+    ]
 
   }
 }

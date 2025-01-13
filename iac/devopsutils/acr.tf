94642bceab7a6d8fa121922ec24eccfa447b5997
David <davejohngoddard@gmail.com>
2025-01-13 09:28:00 +0000

    testing acr ips

--- iac/devopsutils/acr.tf
+++ iac/devopsutils/acr.tf
@@ -1,6 +1,9 @@
+locals {
+  uk_south_acr_azure_ip_ranges = "uk_south.json"
+  uk_west_acr_azure_ip_ranges = "uk_south.json"
+}
+
 resource "null_resource" "fetch_uk_south_acr_ips" {
-
-
   triggers = {
     always_run = timestamp()
   }
@@ -9,19 +12,38 @@ resource "null_resource" "fetch_uk_south_acr_ips" {
     # Logs into the Service principal that has subscription permission over the core network.
     command = <<CMD
       curl -o ips.json https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20250106.json
+      jq -r '.values[] | select(.name == "AzureContainerRegistry.UKSouth") | {ip_ranges: .properties.addressPrefixes}' ips.json > local.uk_south_acr_azure_ip_ranges
+      jq -r '.values[] | select(.name == "AzureContainerRegistry.UKWest") | {ip_ranges: .properties.addressPrefixes}' ips.json > local.uk_west_acr_azure_ip_ranges
     CMD
   }
 }
 
+data "local_file" "dynamic_output" {
+  depends_on = [
+    null_resource.fetch_uk_south_acr_ips
+  ]
+
+  filename = "${path.module}/local.uk_south_acr_azure_ip_ranges"
+}
 
 
 resource "azurerm_container_registry" "devops" {
+  depends_on = [
+    local_file.dynamic_output
+  ]
+
   name = "acr${var.environment_settings.environment}${var.environment_settings.region_code}${var.environment_settings.app_name}"
 
   resource_group_name     = data.azurerm_resource_group.rg.name
   location                = var.environment_settings.region
   sku                     = var.acr_sku
   zone_redundancy_enabled = var.acr_zone_redundancy_enabled
+
+  network_rule_set {
+    default_action = "Deny"
+
+
+  }
 }
 
 resource "azurerm_private_endpoint" "acr" {

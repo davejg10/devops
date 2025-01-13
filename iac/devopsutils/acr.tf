188d6772040084d39fbc7afb9c4af137ec45a28e
David <davejohngoddard@gmail.com>
2025-01-13 11:11:22 +0000

    Creating acr task in local-exec

--- iac/devopsutils/acr.tf
+++ iac/devopsutils/acr.tf
@@ -50,7 +50,7 @@ resource "terraform_data" "create_acr_task" {
     command = <<CMD
       TASK_NAME="build_and_push_custom_image"
       system_identity_principal=$(az acr task create -t $TASK_NAME -n github-task -r ${azurerm_container_registry.devops.name} -c /dev/null -f acb.yaml --auth-mode None --assign-identity [system] --base-image-trigger-enabled false --query "identity.principalId" -o tsv)
-      az role assignment create --assignee $system_identity_principal --role AcrPush --scope ${azurerm_container_registry.devops.id}
+      az role assignment create --role AcrPush --assignee-object-id $system_identity_principal --assignee-principal-type ServicePrincipal --scope ${azurerm_container_registry.devops.id}
       az acr task credential add -r ${azurerm_container_registry.devops.name} -n $TASK_NAME --login-server ${azurerm_container_registry.devops.login_server} --use-identity [system]
     CMD
   }

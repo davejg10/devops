71c01b68c1abefc326d7202f2e4b9d9a767b635c
David <davejohngoddard@gmail.com>
2025-01-13 10:43:11 +0000

    Adding acr task

--- iac/devopsutils/acr.tf
+++ iac/devopsutils/acr.tf
@@ -1,28 +1,36 @@
-locals {
-  uk_south_acr_azure_ip_ranges = jsondecode(file("${path.module}/uk_south.json")).ip_ranges
-  # uk_west_acr_azure_ip_ranges = "${path.cwd}/uk_south.json"
+resource "azurerm_container_registry_task" "build_push_custom_image" {
+  name                  = "build-push-custom-image"
+  container_registry_id = azurerm_container_registry.devops.id
+  platform {
+    os = "Linux"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  registry_credential {
+    source {
+      login_mode = "None"
+    }
+    custom {
+      login_server = azurerm_container_registry.devops.login_server
+      identity = "[system]"
+    }
+  }
+
+
+  file_step {
+    task_file_path = "${path.module}/acb.yaml"
+    context_path = "/dev/null"
+  }
 }
 
-# variable "uk_south_acr_azure_ip_ranges" {
-#   type = string
-#   default = "uk_south.json"
-# }
-
-# resource "null_resource" "fetch_uk_south_acr_ips" {
-#   triggers = {
-#     always_run = timestamp()
-#   }
-
-#   provisioner "local-exec" {
-#     # Logs into the Service principal that has subscription permission over the core network.
-#     command = <<CMD
-#       curl -o ips.json https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20250106.json
-#       jq -r '.values[] | select(.name == "AzureContainerRegistry.UKSouth") | {ip_ranges: .properties.addressPrefixes}' ips.json > local.uk_south_acr_azure_ip_ranges
-#       jq -r '.values[] | select(.name == "AzureContainerRegistry.UKWest") | {ip_ranges: .properties.addressPrefixes}' ips.json > local.uk_west_acr_azure_ip_ranges
-#     CMD
-#   }
-# }
-
+resource "azurerm_role_assignment" "build_push_custom_image" {
+  scope                = azurerm_container_registry.devops.id
+  role_definition_name = "AcrPush"
+  principal_id         = azurerm_container_registry_task.build_push_custom_image.identity[0].principal_id
+}
 
 resource "azurerm_container_registry" "devops" {
   name = "acr${var.environment_settings.environment}${var.environment_settings.region_code}${var.environment_settings.app_name}"
@@ -32,17 +40,7 @@ resource "azurerm_container_registry" "devops" {
   sku                     = var.acr_sku
   zone_redundancy_enabled = var.acr_zone_redundancy_enabled
 
-  network_rule_set {
-    default_action = "Deny"
 
-    ip_rule = [
-      for ip_range in local.uk_south_acr_azure_ip_ranges : {
-        action   = "Allow"
-        ip_range = ip_range
-      } if !strcontains(ip_range, ":")
-    ]
-
-  }
 }
 
 resource "azurerm_private_endpoint" "acr" {
